cc6bfe693a99aadfdb27a00448732c654b20c8d2
JacobLDuncan <jacoblewisduncan@gmail.com>
2019-11-20 17:37:26 -0800

    Layout changes for the views

--- app/src/main/java/com/example/cs370_project/MainActivity.java
+++ app/src/main/java/com/example/cs370_project/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.cs370_project;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
--- app/src/main/java/com/example/cs370_project/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/cs370_project/ui/dashboard/DashboardViewModel.java
@@ -10,7 +10,7 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("Alerts for Geo-Triggers");
     }
 
     public LiveData<String> getText() {
